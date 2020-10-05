5004639e52b0d7321ed779598420fee28fb6dcac
guistetter <guilherme_stetter@hotmail.com>
2020-10-05 11:33:49 -0300

    add item na lista/ entendo estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,39 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJs", "React Native"],
   };
+
+  handleInputChange = (e) => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: "",
+    });
+  };
+
   render() {
     console.log(this.state);
     return (
-      <ul>
-        {this.state.techs.map((tech) => (
-          <li key={tech}>{tech}</li>
-        ))}
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          <h1>{this.state.newTech}</h1>
+          {this.state.techs.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
