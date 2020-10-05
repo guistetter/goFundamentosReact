70b6c64f37f7d3ed93ce6f3d5ec328c68c90fef8
guistetter <guilherme_stetter@hotmail.com>
2020-10-05 12:47:49 -0300

    config propTypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TechItem({ tech, onDelete }) {
   return (
@@ -10,5 +11,12 @@ function TechItem({ tech, onDelete }) {
     </li>
   );
 }
+
+TechItem.defaultProps = { tech: "oculto" };
+
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default TechItem;
